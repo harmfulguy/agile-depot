d168c41a1e01a3db0a910b0ad148139965f30b11
Brennan M. O'Keefe <brennan@considered-harmful.org>
2012-06-29 20:42:48 -0500

    Access control and administration Iteration I4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :password_digest, :password, :password_confirmation
+  after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+private
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise "Can't delete last user"
+    end
+  end
+
 end
+
+
--- test/functional/order_notifier_test.rb
+++ test/functional/order_notifier_test.rb
@@ -6,7 +6,7 @@ class OrderNotifierTest < ActionMailer::TestCase
     assert_equal "Pragmatic Store Order Confirmation", mail.subject
     assert_equal ["dave@example.org"], mail.to
     assert_equal ["depot@example.com"], mail.from
-    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
+    assert_match(/1 x Programming Ruby 1.9/, mail.body.encoded)
   end
 
   test "shipped" do
@@ -14,7 +14,7 @@ class OrderNotifierTest < ActionMailer::TestCase
     assert_equal "Pragmatic Store Order Shipped", mail.subject
     assert_equal ["dave@example.org"], mail.to
     assert_equal ["depot@example.com"], mail.from
-    assert_match /<td>Programming Ruby 1.9<\/td>/, mail.body.encoded
+    assert_match(/<td>Programming Ruby 1.9<\/td>/, mail.body.encoded)
   end
 
 end
